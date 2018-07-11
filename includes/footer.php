f0977e15b0b341bb1bc47302f87f8e9b67208cf6
Tomasl77 <lobgeois.tomas@free.fr>
2018-07-11 14:37:46 +0200

    ajout de qqe trucs

--- includes/footer.php
+++ includes/footer.php
@@ -3,17 +3,17 @@
     <span>
       <h5>Pour mieux nous connaitre</h5>
       <ul>
-        <li>A propos de La boite à musique</li>
-        <li>Qui sommes nous?</li>
+        <li class="pointer" onclick="apropos()">A propos de La boite à musique</li><br>
+        <li class="pointer" onclick="qui()">Qui sommes nous?</li>
       </ul>
     </span>
     <span>
      <h5>Besoin d'aide</h5>
       <ul>
-        <li>F.A.Q</li>
-        <li>Tarifs et option de livraison</li>
-        <li>Retours / Renboursements</li>
-        <li>Aide</li>
+        <li class="pointer" onclick="faq()">F.A.Q</li>
+        <li class="pointer" onclick="tarif()">Tarifs et option de livraison</li>
+        <li class="pointer">Retours / Remboursements</li>
+        <li class="pointer">Aide</li>
       </ul>
     </span>
   </div>
--- index.js
+++ index.js
@@ -27,3 +27,39 @@ var loop = function(){
 }
 setColor();
 loop();
+
+function apropos()
+{
+w=open('','topic','width=350,height=300');
+w.document.write("1. Préambule <br /><br />Le présent document constitue un contrat entre vous et la société. Dans le cadre de ce contrat, nous serons désignés par les termes, « nous », « nos » ou « notre ». Vous serez désigné comme « Utilisateur », « vous », « vos » ou « votre ». \
+L\'Utilisateur est une seule et unique personne physique. Ce contrat couvre votre utilisation du forum, sans distinction du moyen utilisé pour y accéder (que ce soit par exemple sur nos sites Internet, nos applications pour mobiles et tablettes, ou tout autre moyen actuel ou futur).\
+En vous inscrivant sur le forum, vous devez avoir pris connaissance du présent contrat et l\'inscription vaut acceptation de l\'ensemble des dispositions du présent contrat.\
+<br /><br />\
+Nous ne fournissons aucune garantie quant au Service, autres que les garanties légales fournies dans le cadre de la législation applicable.\
+<br /><br />\
+\Le coût de l’utilisation de la technique de communication à distance utilisée est le coût normal facturé à l’Utilisateur par son fournisseur d’accès internet. Ce coût est entièrement supporté par l’Utilisateur.'");
+w.document.close();
+} 
+
+function qui()
+{
+w=open('','topic','width=350,height=200');
+w.document.write("Qui sommes nous? <br /><br /> Nous sommes 4 étudiants de Simplon, une Fabrique de Codeurs solidaires, qui ouvre ses portes a tous, sans distinctions de niveau d'études");
+w.document.close();
+} 
+
+function faq()
+{
+w=open('','topic','width=350,height=200,resizable=no');
+w.document.write("Notre F.A.Q est actuellement en cours de construction.");
+w.document.close();
+}
+
+
+function tarif()
+{
+w=open('','topic','width=350,height=200,resizable=no');
+w.document.write("1. Tarifs : <br /> Nos Tarifs sont indiqués à sur la pages de chaque produits. <br /><br />Livraison : <br />Le tarif de livraison est unique. Il s'élève à 3€ si le montant est inférieur a 30€ d'achat, et est gratuit si l'achat est supérieur ou égal a 30€. ");
+w.document.close();
+}
+
